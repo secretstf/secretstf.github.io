d3496e85b7969d6e18e73d2cadd78172676488a0
secretstf <53250798+secretstf@users.noreply.github.com>
2020-06-11 02:38:02 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			First Test
+		</title>
+	</head>
+	
+	<body>
+		<p>Hello World. My name is Awab Masroor</p>
+		<?php
+			echo "test";
+		?>
+	</body>
+</html>
+© 2020 GitHub, Inc.
